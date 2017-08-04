436d67b4105554694d39d1ea049892cc96464844
wojdziw <wojciech.dziwulski@gmail.com>
2017-08-05 00:48:56 +0200

    some changes

--- assets/js/reducers/order.js
+++ assets/js/reducers/order.js
@@ -4,12 +4,16 @@ import * as types from '../actions/types'
 export const orders = createReducer ({}, {
   [types.SET_ORDERS](state, action) {
     let newOrders = action.orders;
-    for (key in newOrders) {
-      console.log(key);
+    
+    // this is a biiiiig hack. Trying to keep the old state in case db wants to bypass it
+    for (var key in newOrders) {
+      if (key in state) {
+        newOrders[key] = state[key];
+      }
     }
 
     state = action.orders;
-    return state;
+    return newOrders;
 
     
   },
