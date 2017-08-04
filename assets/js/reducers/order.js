0390e2f226cab99a89a0b278f0cd5db54779e695
wojdziw <wojciech.dziwulski@gmail.com>
2017-08-05 00:34:12 +0200

    some changes

--- assets/js/reducers/order.js
+++ assets/js/reducers/order.js
@@ -3,14 +3,17 @@ import * as types from '../actions/types'
 
 export const orders = createReducer ({}, {
   [types.SET_ORDERS](state, action) {
+    let newOrders = action.orders;
+    for (key in newOrders) {
+      console.log(key);
+    }
+
     state = action.orders;
     return state;
+
+    
   },
   [types.SET_STATUS](state, action) {
-    // let all = state;
-    // all[action.id]['status'] = action.status;
-    // return all;
-
     let order = state[action.id];
     order.status = action.status;
     return {...state, [action.id] : order};
